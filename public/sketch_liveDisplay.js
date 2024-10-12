b12d5649d320525c0a1206ddd3d7676e1530cd9f
Mike <smartlifelondon@gmail.com>
2024-10-11 22:33:09 -0400

    Oh baby, C array has been calculated, is on the page, and there is a copy button!

--- public/sketch_liveDisplay.js
+++ public/sketch_liveDisplay.js
@@ -3,15 +3,16 @@
  */
 const ROW_COUNT = 18;
 const COL_COUNT = 54;
-const LED_WIDTH = 25;
-const LED_GAP = 5;
+const SR_COUNT = Math.floor((COL_COUNT-1)/8) + 1; // Number of shift registers required based on row count
+const LED_WIDTH = 20;
+const LED_GAP = 3;
 const BORDER_WIDTH = 25;
 const LED_ON = 1;
 const LED_OFF = 0;
 // Colours
 const LED_OFF_COLOUR = 50;
 const LED_ON_COLOUR = 'red';
-const BACKGROUND_COLOUR = 40;
+const BACKGROUND_COLOUR = 30;
 
 /*
  *  Global Variables
@@ -19,6 +20,7 @@ const BACKGROUND_COLOUR = 40;
 
 // Create 2D array to represent LED display, fill with zeros
 let led_states = Array.from(Array(ROW_COUNT), () => new Array(COL_COUNT).fill(0));
+let c_array = Array.from(Array(ROW_COUNT), () => new Array(SR_COUNT).fill(0));
 
 /*
  *  Setup
@@ -42,7 +44,7 @@ function draw() {
   if(mouseIsPressed) {
     updateDisplay();
   }
-  // Draw all the LEDs in the off state
+  // Draw the LED display
   for (let row = 0; row < ROW_COUNT; row++ ) {
     for(let col = 0; col < COL_COUNT; col++){
       // Set LED position
@@ -57,6 +59,26 @@ function draw() {
       circle(x, y, LED_WIDTH);
     }
   }
+  
+
+  // Update C array of uint8_t to match updated array
+  for (let row = 0; row < ROW_COUNT; row++ ) {
+    for(let sr = 0; sr < SR_COUNT; sr++){
+      // Reset byte value and sum each bit
+      c_array[row][sr] = 0;
+      for(let bit = 0; bit < 8; bit++) {
+        // Make sure we haven't run over the end of the array
+        let bit_index = sr * 8 + bit;
+        if(bit_index < COL_COUNT) {
+          c_array[row][sr] += led_states[row][sr * 8 + bit] * 2 **(7 - bit);
+        }
+      }
+      
+    }
+  }
+  let array_string = c_array.map(row => `{${row.join(', ')}}`).join(',<br>');
+  // Update the content of the <p> tag with id="array"
+  document.getElementById('c_array').innerHTML = 'uint8_t frame_name[ROW_COUNT][SR_COUNT] = {<br>' + array_string + '<br>};';
 }
 
 /*
@@ -75,7 +97,7 @@ function updateDisplay() {
    *  An attempt at improving performance however the draw() time is the bottleneck for refresh rate
    *  Ignoring the gaps between LEDs means dragging the mouse across the display quickly may miss more LEDs then when gaps aren't ignored
    */
-  const IGNORE_GAPS = true;
+  const IGNORE_GAPS = false;
   // Check if mouse click is inside the LED
   if(mouseX > BORDER_WIDTH &&
     mouseX < (COL_COUNT * (LED_WIDTH + LED_GAP) + BORDER_WIDTH - LED_GAP) &&
@@ -145,3 +167,4 @@ function updateDisplay() {
     } // EO Loop through each LED col loop
   } // EO Loop through each LED row loop 
 } // EO updateDisplay()
+
