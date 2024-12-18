0a367b9b6117f94366070166fafa68acf940b31a
Mike <smartlifelondon@gmail.com>
2024-12-17 21:10:15 -0500

    Added websockets. Control of virtual display optimized.

--- public/sketch_liveDisplay.js
+++ public/sketch_liveDisplay.js
@@ -14,6 +14,8 @@ const LED_OFF_COLOUR = 50;
 const LED_ON_COLOUR = 'red';
 const BACKGROUND_COLOUR = 30;
 
+
+
 /*
  *  Global Variables
  */
@@ -21,6 +23,8 @@ const BACKGROUND_COLOUR = 30;
 // Create 2D array to represent LED display, fill with zeros
 let led_states = Array.from(Array(ROW_COUNT), () => new Array(COL_COUNT).fill(0));
 let c_array = Array.from(Array(ROW_COUNT), () => new Array(SR_COUNT).fill(0));
+
+
 
 /*
  *  Setup
@@ -43,6 +47,7 @@ function draw() {
   background(BACKGROUND_COLOUR);
   if(mouseIsPressed) {
     updateDisplay();
+
   }
   // Draw the LED display
   for (let row = 0; row < ROW_COUNT; row++ ) {
@@ -61,36 +66,15 @@ function draw() {
   }
   
 
-  // Update C array of uint8_t to match updated array
-  for (let row = 0; row < ROW_COUNT; row++ ) {
-    for(let sr = 0; sr < SR_COUNT; sr++){
-      // Reset byte value and sum each bit
-      c_array[row][sr] = 0;
-      for(let bit = 0; bit < 8; bit++) {
-        // Make sure we haven't run over the end of the array
-        let bit_index = sr * 8 + bit;
-        if(bit_index < COL_COUNT) {
-          c_array[row][sr] += led_states[row][sr * 8 + bit] * 2 **(7 - bit);
-        }
-      }
-      
-    }
-  }
-  let array_string = c_array.map(row => `{${row.join(', ')}}`).join(',<br>');
-  // Update the content of the <p> tag with id="array"
-  document.getElementById('c_array').innerHTML = 'uint8_t frame_name[ROW_COUNT][SR_COUNT] = {<br>' + array_string + '<br>};';
-}
-
-/*
- *  Toggle LED colour when and LED is clicked
- */
-function mousePressed() {
-  //updateDisplay();
+  
 }
 
 /*
  *  Update display calculates where a mouse is clicked and acts accordingly
  */
+// Set previous values to 1 row/column outside of array
+let prev_mouseX = COL_COUNT; 
+let prev_mouseY = ROW_COUNT;
 function updateDisplay() {
 
   /*
@@ -124,49 +108,51 @@ function updateDisplay() {
       col = col - col_decimal;
       row = row - row_decimal;
     } else {
-      console.log('Error: Invalid "IGNORE_GAPS" =', IGNORE_GAPS);
+      console.log('Error: Invalid "IGNORE_GAPS" value:', IGNORE_GAPS);
       return;
     }
-
+    
     // Left mouse button turns LED on, right mouse button turns LED off
-    if (mouseButton === LEFT) {
+    if (mouseButton === LEFT && led_states[row][col] !== LED_ON) {
       led_states[row][col] = LED_ON;
-    } else if (mouseButton === RIGHT) {
+    } else if (mouseButton === RIGHT && led_states[row][col] !== LED_OFF) {
       led_states[row][col] = LED_OFF;
-    } 
+    }  else {
+      // No change was made, return without updating c_array
+      return;
+    }
+    // There was a change, update c_array
+    updateCArray();
   }  
-  return; // Exit now so you don't have to comment or remove alternate method
-  
-  /*
-   *  This method may or may not be slower, but doesn't really matter since draw() takes much longer to execute
-   *  This method is clearer and easier to read or modify
-   */
-
-  // Loop through each LED
-  for (let row = 0; row < ROW_COUNT; row++ ) {
-    for(let col = 0; col < COL_COUNT; col++){
-      // Set LED minimum position
-      // Uncomment the values on the right to allow gap clicking to turn on/off LEDs
-      let x_min = col * (LED_WIDTH + LED_GAP) + BORDER_WIDTH ;//- 0.5 * LED_GAP;
-      let y_min = row * (LED_WIDTH + LED_GAP) + BORDER_WIDTH ;//- 0.5 * LED_GAP;
-      let x_max = col * (LED_WIDTH + LED_GAP) + BORDER_WIDTH + LED_WIDTH ;//+ 0.5 * LED_GAP;
-      let y_max = row * (LED_WIDTH + LED_GAP) + BORDER_WIDTH + LED_WIDTH ;//+ 0.5 * LED_GAP;
-
-    // Check if the mouse is over an LED
-    if (mouseX > x_min && 
-      mouseX < x_max &&
-      mouseY > y_min && 
-      mouseY < y_max ) {
-        // Left mouse button turns LED on, right mouse button turns LED off
-        if (mouseButton === LEFT) {
-          led_states[row][col] = LED_ON;
-        } else if (mouseButton === RIGHT) {
-          led_states[row][col] = LED_OFF;
-        }
-      } // EO check if mouse is over an LED
-    } // EO Loop through each LED col loop
-  } // EO Loop through each LED row loop 
 } // EO updateDisplay()
+
+/*
+ * Converter led_states to a c array
+ */
+function updateCArray() {
+  // Update C array of uint8_t to match updated array
+  for (let row = 0; row < ROW_COUNT; row++ ) {
+    for(let sr = 0; sr < SR_COUNT; sr++){
+      // Reset byte value and sum each bit
+      c_array[row][sr] = 0;
+      for(let bit = 0; bit < 8; bit++) {
+        // Make sure we haven't run over the end of the array
+        let bit_index = sr * 8 + bit;
+        if(bit_index < COL_COUNT) {
+          c_array[row][sr] += led_states[row][sr * 8 + bit] * 2 **(7 - bit);
+        }
+      }
+      
+    }
+  }
+  let array_string = c_array.map(row => `{${row.join(', ')}}`).join(',<br>');
+  // Update the content of the <p> tag with id="array"
+  document.getElementById('c_array').innerHTML = 'uint8_t frame_name[ROW_COUNT][SR_COUNT] = {<br>' + array_string + '<br>};';
+
+  sendNewDisplayFrame(c_array);
+}
+
+
 
 
 /*
@@ -186,4 +172,5 @@ function liveDisplayReset() {
     led_states[row].fill(0);
     c_array[row].fill(0);
   }
+  updateCArray();
 }
--- server.js
+++ server.js
@@ -1,6 +1,33 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const rpio = require('rpio');
+
+
 let port = 3000;
+let soc; // variable used for sending messages through socket
+
+// Handler for incoming messages from HTML page
+function onConnect(socket) {
+  socket.on("newDisplayFrame", handleNewDisplayFrame);
+  socket.on("requestDisplayFrame", handleRequestDisplayFrame);
+  soc = socket;
+}
+
+/* 
+ * A new frame has been sent from the server
+ * Send this new frame to the microcontroller
+ */
+function handleNewDisplayFrame (message) {
+  console.log("Recieved Message: ", message);
+}
+
+function handleRequestDisplayFrame (message) {
+  console.log("Recieved Message: ", message);
+}
 
 app.use(express.static('public'));
 app.use("/images", express.static('images')); //url now requires /images/filename rather than just /filename
@@ -22,8 +49,8 @@ app.use((error, req, res, next) => {
   });
 });
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running at https://localhost:${port}`);
-
 });
+
+io.sockets.on('connection', onConnect);
